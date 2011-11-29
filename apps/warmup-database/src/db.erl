afee0bc4394b11ec9bc396998dae4322f3c601a0
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-11-29 09:35:16 +0100

    db:new and db:destroy

--- apps/warmup-database/src/db.erl
+++ apps/warmup-database/src/db.erl
@@ -8,8 +8,25 @@
 -export([new/0, destroy/1, write/3, delete/2, read/2, match/2]).
 
 %% @doc ￼db:new() ⇒ Db.
+new() ->
+    [].
+
 %% @doc db:destroy(Db) ⇒ ok.
+destroy(Db) ->
+    ok.
+
 %% @doc db:write(Key, Element, Db) ⇒ NewDb.
+write(Key, Element, Db) ->
+    Db.
+
 %% @doc db:delete(Key, Db) ⇒ NewDb.
+delete(Key, Db) ->
+    Db.
+
 %% @doc db:read(Key, Db) ⇒{ok, Element} | {error, instance}.
+read(Key, Db) ->
+    {error, instance}.
+
 %% @doc db:match(Element, Db) ⇒ [Key1, ..., KeyN].
+match(Element, Db) ->
+    [].
--- apps/warmup-database/test/db_tests.erl
+++ apps/warmup-database/test/db_tests.erl
@@ -1,2 +1,9 @@
 -module(db_tests).
 -include_lib("eunit/include/eunit.hrl").
+
+create_new_db_test() ->
+    ?assertEqual([], db:new()).
+
+destroy_db_test() ->
+    Db = db:new(),
+    ?assertEqual(ok, db:destroy(Db)).
