9ad791f70e7d5140f7e203b464fc950f934cadd0
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-11-29 20:22:04 +0100

    mutex:start and stop + outline for signal/wait

--- apps/warmup-semaphore/src/mutex.erl
+++ apps/warmup-semaphore/src/mutex.erl
@@ -5,16 +5,50 @@
 
 -module(mutex).
 
--export([start/0, wait/0, signal/0]).
+-export([start/0, stop/0, wait/0, signal/0]).
+
+-record(state, {semaphore}).
 
 %% @doc mutex:start() ⇒ ok.
 start() ->
+    register(?MODULE, spawn(fun init/0)),
+    ok.
+
+%% @doc mutex:stop() ⇒ ok.
+stop() ->
+    ?MODULE ! shutdown,
     ok.
 
 %% @doc mutex:wait() ⇒ ok.
 wait() ->
-    ok.
+    Ref = make_ref(),
+    ?MODULE ! {self(), Ref, wait},
+    receive
+        {Ref, Result} -> Result
+    end.
 
 %% @doc mutex:signal() ⇒ ok.
 signal() ->
-    ok.
+    Ref = make_ref(),
+    ?MODULE ! {self(), Ref, signal},
+    receive
+        {Ref, Result} -> Result
+    after 5000 ->
+        {error, timeout}
+    end.
+
+%% @private
+init() ->
+    loop(#state{semaphore=free}).
+
+%% @private
+loop(S) ->
+    receive
+        shutdown ->
+            exit(shutdown);
+        code_change ->
+            ?MODULE:loop(S);
+        Unknown ->
+            error_logger:warning_msg("my_db received unknown message: ~p~n",[Unknown]),
+            loop(S)
+    end.
--- apps/warmup-semaphore/test/mutex_tests.erl
+++ apps/warmup-semaphore/test/mutex_tests.erl
@@ -1,2 +1,46 @@
 -module(mutex_tests).
 -include_lib("eunit/include/eunit.hrl").
+
+start_stop_test() ->
+    mutex:start(),
+    ?assert(lists:member(mutex, registered())),
+
+    mutex:stop(),
+    timer:sleep(100),
+    ?assertNot(lists:member(mutex, registered())).
+
+
+main_test_() ->
+    {foreach,
+     fun setup/0,
+     fun cleanup/1,
+     [
+     ]}.
+
+%% tests with started mutex
+
+
+%% helper
+
+setup() ->
+    mutex:start().
+
+cleanup(Pid) ->
+    mutex:stop(),
+    wait_for_exit(Pid),
+    timer:sleep(100).
+
+wait_for_exit(Pid) ->
+    MRef = erlang:monitor(process, Pid),
+    receive {'DOWN', MRef, _, _, Info} -> Info end.
+
+
+timer() ->
+    timer_loop([]).
+
+timer_loop(Timestamps) ->
+    receive
+        {timing, Timestamp} -> timer_loop(Timestamps ++ [Timestamp]);
+        {Pid, timings}      -> Pid ! {timings, Timestamps};
+        _                   -> timer_loop(Timestamps)
+    end.
